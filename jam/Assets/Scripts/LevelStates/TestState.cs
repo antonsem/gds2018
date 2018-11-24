2961c661874c2880cd859b54244b65347b794ecb
Michal Fibich <Michal.Fibichidis@gmail.com>
2018-11-24 22:27:29 +0100

    - Palette created - Scene for level one has been prepared as example

---
+++ jam/Assets/Scripts/LevelStates/TestState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestState : LevelStateController
+{
+    private float dieTime = 10;
+
+    private void Update()
+    {
+        dieTime -= Time.deltaTime;
+        if (dieTime < 0)
+        {
+            Events.Instance.playerDied.Invoke(DeathType.Explode);
+            Events.Instance.levelCompleted.Invoke();
+        }
+    }
+}
