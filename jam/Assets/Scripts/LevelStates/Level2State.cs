3de2d50d1b7894ea1b5b419695d0b65949a1c9c1
Admin <no-replay@email.com>
2018-11-25 00:40:31 +0100

    Added new level.

---
+++ jam/Assets/Scripts/LevelStates/Level2State.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2State : LevelStateController
+{
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        // After tuch spider you die
+    }
+}
