20a1d9af043e677d1152ad2e6429975d58e754c8
Michal Fibich <Michal.Fibichidis@gmail.com>
2018-11-24 21:39:09 +0100

    - Cursor prefab added

---
+++ jam/Assets/Scripts/CursorSprite.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CursorSprite : MonoBehaviour
+{
+    private GameObject cursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        cursor = GameObject.Find("Cursor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cursor.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cursor.transform.position = new Vector3(cursor.transform.position.x, cursor.transform.position.y, -0.1308594f);
+    }
+}
