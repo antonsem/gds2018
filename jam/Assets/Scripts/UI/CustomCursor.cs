b40b3f161b4bee6b5b586022dba9f03627f5ee55
Michal Fibich <Michal.Fibichidis@gmail.com>
2018-11-25 20:48:41 +0100

    - Custom Cursor for the look and feel

---
+++ jam/Assets/Scripts/UI/CustomCursor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCursor : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    private readonly CursorMode cursorMode = CursorMode.Auto;
+    private Vector2 hotSpot = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+}
