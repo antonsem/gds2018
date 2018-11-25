d20fb8531c1bdb145fcbdc8ed0dc0751dec23a37
Michal Fibich <Michal.Fibichidis@gmail.com>
2018-11-25 17:14:56 +0100

    - Level 3 is now scripted

---
+++ jam/Assets/Scripts/LevelScripts/Level_3/split_head.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class split_head : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Events.Instance.playerDied.Invoke(DeathType.Explode);
+        StartCoroutine(CompleteLevel());
+    }
+
+    IEnumerator CompleteLevel()
+    {
+        yield return new WaitForSeconds(1.75f);
+        Events.Instance.levelCompleted.Invoke();
+    }
+}
